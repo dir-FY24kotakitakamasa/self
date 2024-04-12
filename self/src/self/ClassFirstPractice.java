92140b5d2868bd7fc5062db4ad69ca2b304df667
dir-FY24kotakitakamasa <takamasa.kotaki.v702@dirbato.co.jp>
2024-04-12 19:48:12 +0900

    自習終わり

--- self/src/self/ClassFirstPractice.java
+++ self/src/self/ClassFirstPractice.java
@@ -1,5 +1,33 @@
 package self;
 
 public class ClassFirstPractice {
-
+    public static void main(String[] args) {
+        Employee employee = new Employee();
+        
+        employee.setData(1234, "山田");
+        System.out.println("社員番号：" + employee.getId());
+        System.out.println("社員名：" + employee.getName());
+    }
 }
+
+
+abstract class Employee {
+    int id;
+    String name;
+    
+    public void setData(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    
+  
+    
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+                
+    }
+    
---
+++ self/src/self/Mai.java
@@ -0,0 +1,15 @@
+package self;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+ 
+public class Mai {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 入力パラメータを読み込む
+        String input = br.readLine();
+        
+String[] param = input.split(" ");//入力値を空白で分解する
+        
+    }
+}
